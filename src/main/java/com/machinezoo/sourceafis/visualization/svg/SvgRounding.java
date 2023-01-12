094c7091a8d4d98bf3795ea07cb04f598c3832c8
Michael Wachlhofer <38493853+DerSpeedMann@users.noreply.github.com>
2023-01-12 06:34:33 +0100

    Fixed formatting for non US localization, where comma (,) is used as separator instead of dot (.)

--- src/main/java/com/machinezoo/sourceafis/visualization/svg/SvgRounding.java
+++ src/main/java/com/machinezoo/sourceafis/visualization/svg/SvgRounding.java
@@ -1,18 +1,20 @@
 // Part of SourceAFIS Visualization: https://sourceafis.machinezoo.com/transparency/
 package com.machinezoo.sourceafis.visualization.svg;
 
+import java.util.Locale;
+
 /*
  * Limit number of digits produced into SVG. Screens cannot display smaller differences anyway.
  * Some SVG viewers don't support more than 2 decimal places anyway.
  */
 public class SvgRounding {
 	public static String position(double x) {
-		return String.format("%.2f", x);
+		return String.format(Locale.US,"%.2f", x);
 	}
 	public static String angle(double x) {
 		/*
 		 * 1% of one degree is accurate enough for small markers.
 		 */
-		return String.format("%.2f", x);
+		return String.format(Locale.US, "%.2f", x);
 	}
 }

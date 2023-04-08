7217e73e96dbf2b9aad225f359fbb02d1a49cd9a
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 11:53:58 +0000

    Updated project configuration: stagean links in docs, stagean upgraded

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,51 +1,46 @@
 # This script generates and updates project configuration files.
 
-# We are assuming that project-config is available in sibling directory.
-# Checkout from https://github.com/robertvazan/project-config
-import pathlib
-project_directory = lambda: pathlib.Path(__file__).parent.parent
-config_directory = lambda: project_directory().parent/'project-config'
-exec((config_directory()/'src'/'java.py').read_text())
+# Run this script with rvscaffold in PYTHONPATH
+import rvscaffold as scaffold
 
-project_script_path = __file__
-repository_name = lambda: 'sourceafis-visualization-java'
-pretty_name = lambda: 'SourceAFIS Visualization for Java'
-pom_subgroup = lambda: 'sourceafis'
-pom_artifact = lambda: 'sourceafis-visualization'
-pom_name = lambda: 'SourceAFIS Visualization'
-pom_description = lambda: 'Visualizations of SourceAFIS templates and algorithm transparency data.'
-inception_year = lambda: 2018
-homepage = lambda: website() + 'transparency/'
-jdk_version = lambda: 17
-has_website = lambda: False
-has_javadoc = lambda: False
-stagean_annotations = lambda: True
-project_status = lambda: experimental_status()
-md_description = lambda: '''\
-    SourceAFIS Visualization for Java is a library that generates bitmap and vector images
-    representing [fingerprint templates](https://sourceafis.machinezoo.com/template)
-    and [algorithm transparency](https://sourceafis.machinezoo.com/transparency/) data
-    produced by [SourceAFIS](https://sourceafis.machinezoo.com/) fingerprint recognition engine.
-    This is a Java library, but it can process templates and transparency data from any of the language ports of SourceAFIS.
-'''
+class Project(scaffold.Java):
+    def script_path_text(self): return __file__
+    def repository_name(self): return 'sourceafis-visualization-java'
+    def pretty_name(self): return 'SourceAFIS Visualization for Java'
+    def pom_name(self): return 'SourceAFIS Visualization'
+    def pom_description(self): return 'Visualizations of SourceAFIS templates and algorithm transparency data.'
+    def inception_year(self): return 2018
+    def homepage(self): return self.website() + 'transparency/'
+    def jdk_version(self): return 17
+    def has_website(self): return False
+    def has_javadoc(self): return False
+    def stagean_annotations(self): return True
+    def md_description(self): return '''\
+        SourceAFIS Visualization for Java is a library that generates bitmap and vector images
+        representing [fingerprint templates](https://sourceafis.machinezoo.com/template)
+        and [algorithm transparency](https://sourceafis.machinezoo.com/transparency/) data
+        produced by [SourceAFIS](https://sourceafis.machinezoo.com/) fingerprint recognition engine.
+        This is a Java library, but it can process templates and transparency data from any of the language ports of SourceAFIS.
+    '''
+    
+    def documentation_links(self):
+        yield 'SourceAFIS algorithm transparency', self.homepage()
+    
+    def dependencies(self):
+        yield from super().dependencies()
+        yield self.use('com.machinezoo.sourceafis:sourceafis-transparency:0.13.0')
+        yield self.use_streamex()
+        yield self.use_fastutil()
+        yield self.use_commons_lang()
+        # Lazy way to get convenient SVG DOM builders. Should use dedicated SVG library in the future.
+        yield self.use_pushmode()
+        yield self.use_junit()
+        yield self.use_hamcrest()
+        yield self.use_slf4j_test()
+    
+    def javadoc_links(self):
+        yield from super().javadoc_links()
+        yield 'https://sourceafis.machinezoo.com/javadoc/'
+        # Transparency library does not have javadoc yet.
 
-def documentation_links():
-    yield 'SourceAFIS algorithm transparency', homepage()
-
-def dependencies():
-    use('com.machinezoo.sourceafis:sourceafis-transparency:0.13.0')
-    use_streamex()
-    use_fastutil()
-    use_commons_lang()
-    # Lazy way to get convenient SVG DOM builders. Should use dedicated SVG library in the future.
-    use_pushmode()
-    use_junit()
-    use_hamcrest()
-    use_slf4j_test()
-
-javadoc_links = lambda: [
-    'https://sourceafis.machinezoo.com/javadoc/',
-    # Transparency library does not have javadoc yet.
-]
-
-generate()
+Project().generate()
